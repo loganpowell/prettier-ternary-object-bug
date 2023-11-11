02e3cd08ce300f1f0481d6aed3db8ad13510deea
loganpowell <loganp@tepper.cmu.edu>
2023-11-11 14:30:19 -0500

    bug

---
+++ index.ts
@@ -0,0 +1,14 @@
+const ternaryObject = {
+   ...(true
+      ? { some: 'body', once: 'told', me: 'the', world: 'is' }
+      : {
+           i: "ain't",
+           the: 'sharpest',
+           tool: 'in',
+           da: 'shed',
+           she: 'was',
+           looking: 'kinda',
+           dumb: 'with',
+           her: 'finger',
+        }),
+}
